dd24c1faab70de6c7963c54f2a953f6e752551e5
Adam Reidelbach <adamreidelbach@gmail.com>
2017-07-26 10:12:12 -0500

    product type complete

---
+++ Controllers/ProductTypeController.cs
@@ -0,0 +1,133 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using BangazonAPI.Data;
+using BangazonAPI.Models;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Http;
+
+namespace BangazonAPI.Controllers
+{
+    [Route("api/[controller]")]
+    public class ProductTypeController : Controller
+    {
+
+        private BangazonContext _context;
+        public ProductTypeController (BangazonContext ctx)
+        {
+            _context = ctx;
+        }
+
+        //GET all ProductType from productType table
+        [HttpGet]
+        public IActionResult Get()
+        {
+            IQueryable<object> productTypes = from productType in _context.ProductType select productType;
+
+            if (productTypes == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(productTypes);
+        }
+
+        //GET one productType from productType table
+        [HttpGet("{id}", Name = "GetProductType")]
+        public IActionResult Get([FromRoute] int id)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            try
+            {
+                ProductType productType = _context.ProductType.Single(m => m.ProductTypeId == id);
+
+                if (productType == null)
+                {
+                    return NotFound();
+                }
+                
+                return Ok(productType);
+            }
+            catch (System.InvalidOperationException ex)
+            {
+                return NotFound();
+            }
+        }
+
+        //POST productTypes to the productType table
+        [HttpPost]
+        public IActionResult Post([FromBody] ProductType productType)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            _context.ProductType.Add(productType);
+            
+            try
+            {
+                _context.SaveChanges();
+            }
+            catch (DbUpdateException)
+            {
+                if (ProductTypeExists(productType.ProductTypeId))
+                {
+                    return new StatusCodeResult(StatusCodes.Status409Conflict);
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return CreatedAtRoute("PostProductType", new { id = productType.ProductTypeId }, productType);
+        }
+
+    private bool ProductTypeExists(int productTypeId)
+    {
+      throw new NotImplementedException();
+    }
+
+    // PUT edited values on existing productType
+    [HttpPut("{id}")]
+         public IActionResult Put(int id, [FromBody] ProductType productType)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            if (id != productType.ProductTypeId)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(productType).State = EntityState.Modified;
+
+            try
+            {
+                _context.SaveChanges();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!ProductTypeExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return new StatusCodeResult(StatusCodes.Status204NoContent);
+        }
+    }
+}
