22482dbc72e699127f2b09374dc369fc0439ec36
Adam Reidelbach <adamreidelbach@gmail.com>
2017-07-26 10:17:07 -0500

    forgot to included delete, it is now included

--- Controllers/ProductTypeController.cs
+++ Controllers/ProductTypeController.cs
@@ -129,5 +129,26 @@ namespace BangazonAPI.Controllers
 
             return new StatusCodeResult(StatusCodes.Status204NoContent);
         }
+
+        // DELETE api/producttype/5
+        [HttpDelete("{id}")]
+        public IActionResult Delete(int id)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            ProductType productType = _context.ProductType.Single(m => m.ProductTypeId == id);
+            if (productType == null)
+            {
+                return NotFound();
+            }
+
+            _context.ProductType.Remove(productType);
+            _context.SaveChanges();
+
+            return Ok(productType);
+        }
     }
 }
